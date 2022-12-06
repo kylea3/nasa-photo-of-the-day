8aed37c1fd795d928fe3b527151f5d6789b2a020
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-06 14:43:47 -0700

    add css styling via styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
-
+import styled from 'styled-components';
 import Photo from './components/Photo'
 import Dropdown from './components/Dropdown'
+
+const dummyData = {
+  "date":"2022-12-06",
+  "explanation":"What\u2019s happening inside this interstellar mountain? Stars are forming. The mountain is actually a column of gas and dust in the picturesque Eagle Nebula (M16).  A pillar like this is so low in density that you could easily fly though it -- it only appears solid because of its high dust content and great depth. The glowing areas are lit internally by newly formed stars. These areas shine in red and infrared light because blue light is scattered away by intervening interstellar dust. The featured image was captured recently in near-infrared light in unprecedented detail by the James Webb Space Telescope (JWST), launched late last year. Energetic light, abrasive winds, and final supernovas from these young stars will slowly destroy this stellar birth column over the next 100,000 years.   Astrophysicists: Browse 2,900+ codes in the Astrophysics Source Code Library",
+  "hdurl":"https://apod.nasa.gov/apod/image/2212/M16Pillar_WebbOzsarac_1668.jpg",
+  "media_type":"image",
+  "service_version":"v1",
+  "title":"M16: A Star Forming Pillar from Webb",
+  "url":"https://apod.nasa.gov/apod/image/2212/M16Pillar_WebbOzsarac_960.jpg"
+}
+
+const StyledDiv = styled.div`
+  background-color: #DCD6DO;   
+`;
+
+const Styledp = styled.p`
+  color: orange;
+  font-weight: bold; 
+`;
 
 function App() {
   const [data, setData] = useState();
 
-  useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then(res => {
-      setData(res.data);
-      console.log(res.data);
-    })
-      .catch(err => {
-        console.error(err);
-      })
-  }, []);
+  // remove after finished styling
+  useEffect(() => {setData(dummyData)}, []);
+
+  // useEffect(() => {
+  //   axios.get('https://api.nasa.gov/planetary/apod?api_key=6JdC1yN5x6fpWvEs4VKhxbyI8tCNOSHmDGCxegCm')
+  //   .then(res => {
+  //     setData(res.data);
+  //     console.log(res.data);
+  //   })
+  //     .catch(err => {
+  //       console.error(err);
+  //     })
+  // }, []);
+  
   const options = [
     { value: 5, label: "Five Days Ago"},
     { value: 4, label: "Four Days Ago"},
@@ -28,12 +51,14 @@ function App() {
   
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-      { data && <Dropdown placeHolder={data.date} options={options} /> }
-      { data && <Photo hdurl={data.hdurl} date={data.date} title={data.title} explanation={data.explanation} /> }
+      <StyledDiv> 
+        <Styledp>
+          Choose another date from the dropdown menu to see the photo from that 
+        day!
+        </Styledp>
+        { data && <Dropdown placeHolder={data.date} options={options} /> }
+        { data && <Photo hdurl={data.hdurl} date={data.date} title={data.title} explanation={data.explanation} /> }
+      </StyledDiv>
     </div>
   );
 
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,12 +1,49 @@
 import React from "react";
+import styled, { keyframes } from 'styled-components';
 
+const HeadingDiv = styled.div`
+    background-color: #D37506;
+    border: 3px solid black;
+`;
+
+const kf = keyframes`
+        from { width: 0 }
+        to { width: 100% }
+}
+`
+
+const StyledH2 = styled.h2`
+    color: #fff;
+    overflow: hidden; 
+    white-space: nowrap; 
+    margin: 0 auto; 
+    letter-spacing: .15em; 
+    animation: ${kf} 3.5s steps(30, end);
+    padding-top: 5px;
+    margin: 0px;
+`;
+
+const Styledp = styled.p`
+    color: #fff;
+    text-align: center;
+    overflow: hidden;
+    white-space: nowrap; 
+    margin: 0 auto; 
+    letter-spacing: .15em; 
+    animation: ${kf} 3.5s steps(30, end);
+    padding-bottom: 5px;
+    margin: 0;
+    font-weight: bold;
+`
 
 const Photo = (props) => {
     return (
-    <div>
+    <div>    
         <img src={props.hdurl} />
-        <h2>{props.title}</h2>
-        <p>{props.date}</p>
+        <HeadingDiv>
+            <StyledH2>{props.title}</StyledH2>
+            <Styledp>{props.date}</Styledp>
+        </HeadingDiv>    
         <p>{props.explanation}</p>
     </div>
     )
